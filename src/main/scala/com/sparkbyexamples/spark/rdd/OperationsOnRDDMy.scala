d16b939a6219354720720e7abb7aa1add8489aa9
Thomas Young <yk_ecust_2007@163.com>
2020-10-03 20:46:24 +0800

    sort

--- src/main/scala/com/sparkbyexamples/spark/rdd/OperationsOnRDDMy.scala
+++ src/main/scala/com/sparkbyexamples/spark/rdd/OperationsOnRDDMy.scala
@@ -5,7 +5,7 @@ import org.apache.spark.sql.SparkSession
 object OperationsOnRDDMy extends App {
   val spark = SparkSession.builder()
     .appName("OperationsOnRDDMy")
-    .master("local")
+    .master("local[1]")
     .getOrCreate()
 
   spark.sparkContext.setLogLevel("ERROR")
@@ -15,4 +15,12 @@ object OperationsOnRDDMy extends App {
   )
   val listRdd = spark.sparkContext.parallelize(List(9,2,3,4,5,6,7,8))
 
+  val wordRdd = rdd.flatMap(_.split(" "))
+  //sortBy
+  println("Sort by word name")
+  val sortRdd = wordRdd.sortBy(f=>f)
+  sortRdd.foreach(println)
+  println
+  val groupRdd = wordRdd.groupBy(f=>f.length)
+  groupRdd.sortBy(f=>f._1).foreach(println)
 }
