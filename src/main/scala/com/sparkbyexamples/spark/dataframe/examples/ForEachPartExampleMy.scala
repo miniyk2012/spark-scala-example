085f89441dd29bb716b6ec5d9002b439b9fcd5f6
杨恺 <yangkai02@qutoutiao.net>
2020-10-12 15:33:31 +0800

    mapPartition

--- src/main/scala/com/sparkbyexamples/spark/dataframe/examples/ForEachPartExampleMy.scala
+++ src/main/scala/com/sparkbyexamples/spark/dataframe/examples/ForEachPartExampleMy.scala
@@ -1,5 +1,57 @@
 package com.sparkbyexamples.spark.dataframe.examples
 
+import org.apache.spark.sql.SparkSession
+
 object ForEachPartExampleMy {
+  def main(args: Array[String]): Unit = {
+    val spark: SparkSession = SparkSession.builder()
+      .master("local[1]")
+      .appName("SparkByExamples.com")
+      .getOrCreate()
+
+    spark.sparkContext.setLogLevel("WARN")
+
+    val data = Seq(("Banana", 1000, "USA"), ("Carrots", 1500, "USA"), ("Beans", 1600, "USA"),
+      ("Orange", 2000, "USA"), ("Orange", 2000, "USA"), ("Banana", 400, "China"),
+      ("Carrots", 1200, "China"), ("Beans", 1500, "China"))
+
+    val df = spark.createDataFrame(data).repartition(3).toDF("Product", "Amount", "Country")
+    val i = spark.sparkContext.longAccumulator("i")
+    df.foreachPartition(partitionOfRecords => {
+      i.add(1)
+      partitionOfRecords.foreach(println)
+    })
+    println(s"i=${i.value}, ${i}")
+
+    println
+    //rdd
+    val rdd = spark.sparkContext.parallelize(Seq(1, 2, 3, 4, 5, 6, 7, 8, 9), 5)
+    rdd.mapPartitions(r => r).foreach(println)
+
+    println
+    val j = spark.sparkContext.longAccumulator("j")
+    rdd.mapPartitions(records => {
+      j.add(1)
+      records
+    }).foreach(println)
+    println(s"i=${j.value}, ${j}")  // 5个partition, j只能在driver端读取
+
+    println
+    var n = 0
+    val newrdd = rdd.map(r => {
+      n += 1
+      r
+    })
+    newrdd.count()
+    println("n=" + n)  // n=0, 因为driver中的变量没办法被executor中改变
+
+    println
+    val newrdd2 = rdd.map(r => {
+      j.add(1)
+      r
+    })
+    newrdd2.count()
+    println(s"i=${j.value}, ${j}")  // 可以重复累加
+  }
 
 }
--- src/main/scala/com/sparkbyexamples/spark/dataframe/examples/MapTransformationMy.scala
+++ src/main/scala/com/sparkbyexamples/spark/dataframe/examples/MapTransformationMy.scala
@@ -1,5 +1,59 @@
 package com.sparkbyexamples.spark.dataframe.examples
 
-object MapTransformationMy {
+import org.apache.spark.sql.types.{IntegerType, StringType, StructType}
+import org.apache.spark.sql.{Row, SparkSession}
 
+object MapTransformationMy {
+  def main(args: Array[String]): Unit = {
+    val spark:SparkSession = SparkSession.builder()
+      .master("local[5]")
+      .appName("SparkByExamples.com")
+      .getOrCreate()
+
+    spark.sparkContext.setLogLevel("WARN")
+
+    val structureData = Seq(
+      Row("James","","Smith","36636","NewYork",3100),
+      Row("Michael","Rose","","40288","California",4300),
+      Row("Robert","","Williams","42114","Florida",1400),
+      Row("Maria","Anne","Jones","39192","Florida",5500),
+      Row("Jen","Mary","Brown","34561","NewYork",3000)
+    )
+
+    val structureSchema = new StructType()
+      .add("firstname",StringType)
+      .add("middlename",StringType)
+      .add("lastname",StringType)
+      .add("id",StringType)
+      .add("location",StringType)
+      .add("salary",IntegerType)
+
+    val df2 = spark.createDataFrame(
+      spark.sparkContext.parallelize(structureData),structureSchema)
+    df2.printSchema()
+    df2.show(false)
+    import spark.implicits._
+
+    val util = new Util()
+    val df3 = df2.map(row=>{
+      // 这里需要把util序列化到各个executor中, 因此比较费cpu和io
+      val fullName = util.combine(row.getString(0),row.getString(1),row.getString(2))
+      (fullName, row.getString(3), row.getInt(5))
+    })
+    val df3Map = df3.toDF("fullName", "id", "salary")
+    df3Map.printSchema()
+    df3Map.show(false)
+
+    val df4 = df2.mapPartitions(iter=> {
+      val util = new Util()
+      val res = iter.map(row=>{
+        val fullName = util.combine(row.getString(0),row.getString(1),row.getString(2))
+        (fullName, row.getString(3), row.getInt(5))
+      })
+      res
+    })
+    val df4Part = df3.toDF("fullName", "id", "salary")
+    df4Part.printSchema()
+    df4Part.show(false)
+  }
 }
